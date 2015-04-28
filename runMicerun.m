2cc878e03026f4ddde9ba2423ffb3dbc2e72cc64
Elena Mahrt <elena.mahrt@email.wsu.edu>
2015-04-28 09:45:46 -0700

    draft of making mice_predictions a fxn: runMicerun.m

---
+++ runMicerun.m
@@ -0,0 +1,8 @@
+
+
+
+run_predictions_mice('distorted4', 'mice_clean.txt', 'stimuliDist.txt')
+
+run_predictions_mice('distorted10', 'mice_clean.txt', 'stimuliDist.txt')
+
+run_predictions_mice('distorted20', 'mice_clean.txt', 'stimuliDist.txt')
