b970e11893ba2bfff60820791dce81829180c411
Elena Mahrt <elena.mahrt@email.wsu.edu>
2015-05-08 15:02:00 -0700

    miceError arrays not made correctly. Trying to debug

--- run_predictions_mice.m
+++ run_predictions_mice.m
@@ -18,7 +18,7 @@ close all
 % clear all
 
 %%%%%%  SET UP NOTES   %%%%%%
-% Check: Turn on/off HP filters in 'GenerateStimulus.m'
+% Check: Turn on/off LP filters in 'GenerateStimulus.m'
 % Check: how many stimuli are you analyzing? in 'yourStim' variable
 % Check: Which calculation of prediction error do you want to use? in
 % 'VisualizeTracePredictions.m'
@@ -30,20 +30,22 @@ savepath = [startpath, Model, '\']
 %Load your test information
 mousedata = dlmread(mice,'\t');
 
-%Define some parameter and variable sizes
-numColumns = length(mousedata);
-numMice = size(mousedata,1);
 
 %Load your stimuli information
 yourStim = 33; %How many stimuli do you have? (i.e. how many rows in 'stimuli.txt' or 'stimuliDist.txt' do you have?)
+
 fileID=fopen(stim); %input from function 'stim'
-stimdata = textscan(fileID, '%s', yourStim); 
+stimdata = textscan(fileID, '%s', yourStim);
 fclose(fileID);
+
+%Define some parameter and variable sizes
+numMice = size(mousedata,1); %number of rows in your text file
+numColumns = (yourStim+2); %Is the number of stimuli plus two extra columns for the mouse # and cell depth (cell identifiers used in output text files)
 
 % Create empty arrays to put model information into
 micePrediction = zeros(numMice, numColumns); %This makes an array the same size as the number of files to be analyzed and fills it with zeros
 miceError = zeros(numMice, numColumns);  %This makes an array the same size as the number of files to be analyzed and fills it with zeros
-sndType = '.call1'; %what is your stimulus file type?
+% sndType = '.call1'; %what is your stimulus file type?
 % sndType = '.wav';
 
 if exist('C:\Users\emahrt\Documents\mice_predictions\Output\Mouse')
@@ -108,11 +110,12 @@ for mouse = 1:numMice
     % % VisualizeTraceData(experiment_data,prefs,test_num,trace_num,[0 0 0 1 1 1 1 0 0 0]);
     % %  VisualizeTraceData(experiment_data,prefs,test_num,trace_num,[0 0 0 0 0 0 0 1 1 0]);
     % % end
-        
+    
     % --------------------------------
     % Model Generation and Prediction
     % --------------------------------
     %Specify the tests to use as training data
+    %     disp('mousedata MouseNum and depth')
     micePrediction(mouse, 1) = mousedata(mouse,1); %%puts the mouse # in the first column of output .txt files
     micePrediction(mouse, 2) = mousedata(mouse,2); %puts the cell depth in the first column of output .txt files
     miceError(mouse, 1) = mousedata(mouse,1); %puts the mouse # in the first column of output .txt files
@@ -130,60 +133,63 @@ for mouse = 1:numMice
         close all
         
         %Visualize prediction made from the model on a specified trace
-        for column =1:(numColumns-9) %vocalization test #s start in the 10th column and extend to a maximum of 33 columns beyond that.
-            Vocal = mousedata(mouse,9+column);
-            if (Vocal ~= 0)
-                for testNum = Vocal
-                    vocalStr = experiment_data.test(1,testNum).trace(1,1).stimulus.vocal_call_file;
-                    vocalNum=0;
+        %         for column =1:(length(mousedata)-9) %vocalization test #s start in the 10th column and extend to a maximum of 33 columns beyond that.
+        for column =1:yourStim % loop through all the vocalization test numbers.
+            
+            testNum = mousedata(mouse,9+column); %grab the test number; mouse is the row and the vocalization test numbers start in the 10th column (thus 9+ column)
+            if (testNum ~= 0) %if there is a test number, then do the next step
+%                 if length(testNum) >1
+%                     error('you have more than one test number')
+%                 end
+            %Assign a number to each of the stimuli used
+                vocalStr = experiment_data.test(1,testNum).trace(1,1).stimulus.vocal_call_file; %grabs the name of the stimulus file from the Batlab file in testNum
+                vocalNum=0;           
+                stim = stimdata{1,1};
+                vocalNum = find(strcmp(stim,vocalStr)); %find the index of the location where the matching vocalfile name is in stimdata
+                
+                if vocalNum~=0
+                    test_to_view = testNum;
+                    trace_num = size(experiment_data.test(1,test_to_view).trace, 2);
+                    trace_to_view = trace_num; % trace=1, arrn=40; trace=2, arrn=30; trace=3, arrn=20;
                     
-                    %Assign a number to each of the stimuli used
-                    stim = stimdata{1,1};
-                    vocalNum = find(strcmp(stim,vocalStr)); %find the index of the location where the matching vocalfile name is in stimdata
+                    [model_errorsL, model_output] = VisualizeTracePredictions(experiment_data, ...
+                        prefs, ...
+                        model, ...
+                        test_to_view, ...
+                        trace_to_view,[]);
+                    numStr = num2str(test_to_view);
+                    figname = [savepath prefs.cell_id '_vocalL_' numStr '.pdf'];
+                    saveas(gcf,figname);
+                    %                                 close all
+                    close force all
                     
-                    if vocalNum~=0
-                        test_to_view = testNum;
-                        trace_num = size(experiment_data.test(1,test_to_view).trace, 2);
-                        trace_to_view = trace_num; % trace=1, arrn=40; trace=2, arrn=30; trace=3, arrn=20;
-                        
-                        [model_errorsL, model_output] = VisualizeTracePredictions(experiment_data, ...
-                            prefs, ...
-                            model, ...
-                            test_to_view, ...
-                            trace_to_view,[]);
-                        numStr = num2str(test_to_view);
-                        figname = [savepath prefs.cell_id '_vocalL_' numStr '.pdf'];
-                        saveas(gcf,figname);
-                        %                                 close all
-                        close force all
-                        
-                        micePrediction(mouse, vocalNum+2) = sum(max(model_output,0));
-                        miceError(mouse, vocalNum+2) = model_errorsL;
-                        lmo = length(model_output);
-                        if lmo<100 model_output = [model_output model_output(lmo)*ones(1,100-lmo)]; end %100 has to do with the length of the model; because the model only predicts for the length of the stimulus, not after.
-                        if lmo>100 model_output = model_output(1:100); end
-                        responsePrediction(vocalNum, :) = model_output;
-                        %             prediction( mouse, testNum ) = sum( max(model_output,0))/(size(model_output, 2));
-                        %             [model_errorsLC model_output] = VisualizeTracePredictions(experiment_data, ...
-                        %                                       prefs, ...
-                        %                                       constrModel, ...
-                        %                                       test_to_view, ...
-                        %                                       trace_to_view);
-                        %             numStr = num2str(test_to_view);
-                        % %             figname = ['/Users/robertpa/Desktop/mouseDataFigs/' prefs.cell_id '_vocalLC_' numStr '.pdf'];
-                        % %             saveas(gcf,figname);
-                        close all
-                        display(['prediction = ' num2str(micePrediction(mouse, vocalNum+2 )) ', model_errorsL = ' num2str(model_errorsL)]);
-                        %             display(['model_errorsL = ' num2str(model_errorsL) ', model_errorsLC = ' num2str(model_errorsLC)]);
-                    end
+                    micePrediction(mouse, vocalNum+2) = sum(max(model_output,0)); % put the value into the mouse' row. The column is the same as the index of the vocalStr. +2 to skip mouse# and depth columns
+                    miceError(mouse, vocalNum+2) = model_errorsL;
+                    lmo = length(model_output);
+                    if lmo<100 model_output = [model_output model_output(lmo)*ones(1,100-lmo)]; end %zero padding; 100 has to do with the length of the model; because the model only predicts for the length of the stimulus, not after.
+                    if lmo>100 model_output = model_output(1:100); end
+                    responsePrediction(vocalNum, :) = model_output;
+                    %             prediction( mouse, testNum ) = sum( max(model_output,0))/(size(model_output, 2));
+                    %             [model_errorsLC model_output] = VisualizeTracePredictions(experiment_data, ...
+                    %                                       prefs, ...
+                    %                                       constrModel, ...
+                    %                                       test_to_view, ...
+                    %                                       trace_to_view);
+                    %             numStr = num2str(test_to_view);
+                    % %             figname = ['/Users/robertpa/Desktop/mouseDataFigs/' prefs.cell_id '_vocalLC_' numStr '.pdf'];
+                    % %             saveas(gcf,figname);
+                    close all
+                    display(['prediction = ' num2str(micePrediction(mouse, vocalNum+2 )) ', model_errorsL = ' num2str(model_errorsL)]);
+                    %             display(['model_errorsL = ' num2str(model_errorsL) ', model_errorsLC = ' num2str(model_errorsLC)]);
+                    
                 end
             end
         end
     end
     
-    dlmwrite(strcat(savepath, '\responsePrediction.txt'), responsePrediction, 'delimiter', '\t', 'precision', '%.4f', '-append')
-    dlmwrite(strcat(savepath, '\micePrediction.txt'), micePrediction, 'delimiter', '\t', 'precision', '%.4f', '-append')
-    dlmwrite(strcat(savepath, '\micePredictError.txt'), miceError, 'delimiter', '\t', 'precision', '%.4f', '-append')
+    dlmwrite(strcat(savepath, '\responsePrediction.txt'), responsePrediction, 'delimiter', '\t', 'precision', '%.4f') % add '-append' if you want to append onto previous run throughs of the files
+    dlmwrite(strcat(savepath, '\micePrediction.txt'), micePrediction, 'delimiter', '\t', 'precision', '%.4f')
+    dlmwrite(strcat(savepath, '\micePredictError.txt'), miceError, 'delimiter', '\t', 'precision', '%.4f')
     
 end
 
